378286fefe019cb07d0b712a5568f2bbacfa49aa
VikVp <notblonde7@yandex.ru>
2020-05-23 13:53:11 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function () {
   var mixer = mixitup('.products__inner-box');
 
+  $('.slider__inner').slick({
+
+    dots: false,
+    infinite: true,
+    speed: 500,
+    // fade: true,
+    // cssEase: 'linear'
+  });
 });
