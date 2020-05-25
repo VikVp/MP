9a102f50eb3134e3f20c285cd1a383182e90342e
VikVp <notblonde7@yandex.ru>
2020-05-25 19:15:51 +0300

    feed

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,27 @@
 $(function () {
-  var mixer = mixitup('.products__inner-box');
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"></path></svg>'
 
-  $('.slider__inner').slick({
+  });
 
+  var mixer = mixitup('.products__inner-items');
+
+  $('.weekly__inner').slick({
     dots: false,
     infinite: true,
     speed: 500,
+    // fade: true,
+    // cssEase: 'linear'
+  });
+
+  $('.feed__inner').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
     // fade: true,
     // cssEase: 'linear'
   });
