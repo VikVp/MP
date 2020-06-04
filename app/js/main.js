54f6948a11406b94766d8f04122a755b7e4df5b6
VikVp <notblonde7@yandex.ru>
2020-06-04 15:46:33 +0300

    adaptive main

--- app/js/main.js
+++ app/js/main.js
@@ -2,23 +2,32 @@ $(function () {
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px",
-    // normalFill: "#ffc000",
-    // ratedFill: "#E74C3C",
-    // starSvg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"></path></svg>'
-    // starSvg: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
-    // starSvg: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
-    // starSvg: '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><rect fill="none" height="24" width="24" x="0"/></g><g><g><g><path d="M22,9.24l-7.19-0.62L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27L18.18,21l-1.63-7.03L22,9.24z M12,15.4V6.1 l1.71,4.04l4.38,0.38l-3.32,2.88l1,4.28L12,15.4z"/></g></g></g></svg>'
-    // starSvg: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path fill="blue" d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"/></svg>'
-
   });
 
-  var mixer = mixitup('.products__inner-items');
+
 
   $('.weekly__inner').slick({
     dots: false,
     infinite: true,
     speed: 500,
-
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      // {
+      //   breakpoint: 941,
+      //   settings: {
+      //     slidesToShow: 2,
+      //     slidesToScroll: 2
+      //   }
+      // },
+      {
+        breakpoint: 641,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
   });
 
   $('.feed__inner').slick({
@@ -26,8 +35,34 @@ $(function () {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3
-    // variableWidth: true
+    slidesToScroll: 3,
+    // variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1140,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      ,
+      {
+        breakpoint: 641,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
   });
 
   $('.feedback__inner').slick({
@@ -35,12 +70,36 @@ $(function () {
     infinite: true,
     speed: 500,
     slidesToShow: 2,
-    slidesToScroll: 2
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 941,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 641,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
   });
 
 
-  $('.products__filter-btn').on('click', function () {
+  // $('.products__filter-btn').on('click', function () {
+  //   $('.products__list').slideToggle();
+  // });
+  $('.products__filter').on('click', function () {
     $('.products__list').slideToggle();
   });
+
+
+  var mixer = mixitup('.products__inner-items');
 });
 
